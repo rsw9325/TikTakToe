4a5475178928b9d622129956cc21409f624c38ef
rahulsw00 <rahulsw12234@gmail.com>
2023-09-14 17:11:37 +0530

    Update tiktaktoe.py

--- tiktaktoe.py
+++ tiktaktoe.py
@@ -2,5 +2,14 @@ from tkinter import *
 window = Tk()
 
 window.title("TikTakToe")
-window.geometry("300x200+10+20")
+width = w = 1024 # Width 
+height = h =  640 # Height
+
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = (screen_width/2) - (width/2)
+y = (screen_height/2) - (height/2)
+
+window.geometry('%dx%d+%d+%d' % (w, h, x, y))
 window.mainloop()
