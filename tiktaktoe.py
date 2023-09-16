1f076c284dcdbbbcfd319134182ccd09b52a3dc4
rahulsw00 <rahulsw12234@gmail.com>
2023-09-16 19:28:49 +0530

    Update tiktaktoe.py

--- tiktaktoe.py
+++ tiktaktoe.py
@@ -13,31 +13,38 @@ y = (screen_height/2) - (h/2)
 
 window.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-btn_1 = Button(window, text = "1", height = 4, width = 8)
+canvas = Canvas(window, height = h, width = w)
+canvas.pack()
+canvas.create_rectangle(619, 204, 404, 436, fill = "#000000")
+
+def o():
+        print('no')
+
+btn_1 = Button(window, text = "1", height = 4, width = 8, bd = 0)
 btn_1.place(relx = 0.425, rely = 0.37, anchor = CENTER)
 
-btn_2 = Button(window, text = "2", height = 4, width = 8)
+btn_2 = Button(window, text = "2", height = 4, width = 8, bd = 0)
 btn_2.place(relx = 0.5, rely = 0.37, anchor = CENTER)
 
-btn_3 = Button(window, text = "3", height = 4, width = 8)
+btn_3 = Button(window, text = "3", height = 4, width = 8, bd = 0)
 btn_3.place(relx = 0.575, rely = 0.37, anchor = CENTER)
 
-btn_4 = Button(window, text = "4", height = 4, width = 8)
+btn_4 = Button(window, text = "4", height = 4, width = 8, bd = 0)
 btn_4.place(relx = 0.425, rely = 0.5, anchor = CENTER)
 
-btn_5 = Button(window, text = "5", height = 4, width = 8)
+btn_5 = Button(window, text = "5", height = 4, width = 8, bd = 0)
 btn_5.place(relx = 0.5, rely = 0.5, anchor = CENTER)
 
-btn_6 = Button(window, text = "6", height = 4, width = 8)
+btn_6 = Button(window, text = "6", height = 4, width = 8, bd = 0)
 btn_6.place(relx = 0.575, rely = 0.5, anchor = CENTER)
 
-btn_7 = Button(window, text = "7", height = 4, width = 8)
+btn_7 = Button(window, text = "7", height = 4, width = 8, bd = 0)
 btn_7.place(relx = 0.425, rely = 0.63, anchor = CENTER)
 
-btn_8 = Button(window, text = "8", height = 4, width = 8)
+btn_8 = Button(window, text = "8", height = 4, width = 8, bd = 0)
 btn_8.place(relx = 0.5, rely = 0.63, anchor = CENTER)
 
-btn_9 = Button(window, text = "9", height = 4, width = 8)
+btn_9 = Button(window, text = "9", height = 4, width = 8, bd = 0)
 btn_9.place(relx = 0.575, rely = 0.63, anchor = CENTER)
 
 
